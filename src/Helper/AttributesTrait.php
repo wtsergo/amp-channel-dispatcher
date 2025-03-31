a0e5c2e2c4d35866504f0b33f6a2e7983b66b3b0
sergey boyko <wtsergo@gmail.com>
2025-03-31 14:09:11 -0400

    Renamed $attributes property to $__attributes in AttributesTrait

--- src/Helper/AttributesTrait.php
+++ src/Helper/AttributesTrait.php
@@ -4,23 +4,23 @@ namespace Wtsergo\AmpChannelDispatcher\Helper;
 
 trait AttributesTrait
 {
-    protected array $attributes = [];
+    protected array $__attributes = [];
     public function getAttributes(): array
     {
-        return $this->attributes;
+        return $this->__attributes;
     }
-    public function setAttributes(array $attributes): static
+    public function setAttributes(array $__attributes): static
     {
-        $this->attributes = $attributes;
+        $this->__attributes = $__attributes;
         return $this;
     }
     public function getAttribute(string $name): mixed
     {
-        return $this->attributes[$name] ?? null;
+        return $this->__attributes[$name] ?? null;
     }
     public function setAttribute(string $name, mixed $value): static
     {
-        $this->attributes[$name] = $value;
+        $this->__attributes[$name] = $value;
         return $this;
     }
 }
